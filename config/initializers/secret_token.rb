192cfb1f835cc930938d1aa26a0d631f502e10fe
Xing Zhou <xing@xingzhou.me>
2012-09-03 19:19:58 -0400

    Initial commit with new Rails structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fwl::Application.config.secret_token = '0e3d2cae2ff4bb9016b6d55ad72d0b994ca4f3e459c5f2d78f692aa821e41a776d7a5085e641da67ea9ebee6b0ab520f3dec97d76257696751876a7b7bb705ad'
